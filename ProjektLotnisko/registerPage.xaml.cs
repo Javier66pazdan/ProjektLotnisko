1421bea49469f645957e684476ed925ae6a181a0
Gabriel <Gabriel.Pazdan1@gmail.com>
2021-06-21 17:37:35 +0200

    Prosta walidacja

--- ProjektLotnisko/registerPage.xaml.cs
+++ ProjektLotnisko/registerPage.xaml.cs
@@ -23,7 +23,6 @@ namespace ProjektLotnisko
     {
 
         AirportManagementContext db;
-
         public registerPage()
         {
             InitializeComponent();
@@ -42,36 +41,51 @@ namespace ProjektLotnisko
             loginWind.ShowDialog();
         }
 
+        protected void register()
+        {
+            User nowy = new User()
+            {
+                Email = emailField.Text,
+                Password = passwordField1.Text,
+                FirstName = nameField.Text,
+                LastName = surnameField.Text,
+                SignUpDate = DateTime.Now,
+                AdressStreet = adressStreetField.Text,
+                AdressNumber = adressNumberField.Text,
+                Country = "Poland"
+            };
+
+            db.Users.Add(nowy);
+            db.SaveChanges();
+            MessageBox.Show("Pomyślnie utworzono konto");
+        }
+
         private void registerBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (passwordField.Text != passwordField1.Text)
+            if (nameField.Text == "" || surnameField.Text == "" || emailField.Text == "" ||
+                passwordField.Text == "" || passwordField1.Text == "" || adressStreetField.Text == "" ||
+                adressNumberField.Text == "")
+            {
+                MessageBox.Show("Pola nie mogą być puste!");
+            }
+            else if (nameField.Text == "Imie" || surnameField.Text == "Nazwisko" || emailField.Text == "Email" ||
+              passwordField.Text == "Haslo" || passwordField1.Text == "Powtorz haslo" || adressStreetField.Text == "Ulica" ||
+              adressNumberField.Text == "numer domu")
+            {
+                MessageBox.Show("Wprowadz prawidlowe dane");
+            }
+            else if (passwordField.Text != passwordField1.Text)
             {
                 MessageBox.Show("Podaj takie same hasła");
             }
             else
             {
-                User nowy = new User()
-                {
-                    Email = emailField.Text,
-                    Password = passwordField1.Text,
-                    FirstName = nameField.Text,
-                    LastName = surnameField.Text,
-                    SignUpDate = DateTime.Now,
-                    AdressStreet = adressStreetField.Text,
-                    AdressNumber = adressNumberField.Text,
-                    Country = "Poland"
-                };
-
-                db.Users.Add(nowy);         
-                db.SaveChanges();
-                MessageBox.Show("Pomyślnie utworzono konto");
-              
+                registerPage objekt2 = new registerPage();
+                objekt2.register();
                 MainWindow logowanie = new MainWindow();
                 this.Close();
                 logowanie.Show();
             }
-
-
         }
     }
 }
